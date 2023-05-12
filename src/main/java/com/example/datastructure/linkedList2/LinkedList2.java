e4d8dc3ebff584ac07af5f5b7e8d954dc4948fca
Si Thu Win <sthuwin@gmail.com>
2023-05-12 12:12:00 +1000

    sudokusolver

--- src/main/java/com/example/datastructure/linkedList2/LinkedList2.java
+++ src/main/java/com/example/datastructure/linkedList2/LinkedList2.java
@@ -1,2 +1,89 @@
-package com.example.datastructure.linkedlist;public class LinkedList2 {
+package com.example.datastructure.linkedList2;
+
+public class LinkedList2 {
+  private Node head;
+  private Node tail;
+  private int length;
+
+  class Node {
+    int value;
+    Node next;
+
+    Node(int value) {
+      this.value = value;
+    }
+  }
+
+  public LinkedList2(int value) {
+    Node newNode = new Node(value);
+    head = newNode;
+    tail = newNode;
+    length = 1;
+  }
+
+  public void printList() {
+    Node temp = head;
+    while (temp != null) {
+      System.out.println(temp.value);
+      temp = temp.next;
+    }
+  }
+
+  public void getHead() {
+    if (head == null) {
+      System.out.println("Head: null");
+    } else {
+      System.out.println("Head: " + head.value);
+    }
+  }
+
+  public void getTail() {
+    if (head == null) {
+      System.out.println("Tail: null");
+    } else {
+      System.out.println("Tail: " + tail.value);
+    }
+  }
+
+  public void getLength() {
+    System.out.println("Length: " + length);
+  }
+
+  public void append(int value) {
+    Node newNode = new Node(value);
+    if (length == 0) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      tail = newNode;
+    }
+    length++;
+  }
+
+  public void makeEmpty() {
+    head = null;
+    tail = null;
+    length = 0;
+  }
+
+  public Node removeLast() {
+    if (length == 0) return null;
+    Node temp = head;
+    Node pre = head;
+    while(temp.next != null) {
+      pre = temp;
+      temp = temp.next;
+    }
+    tail = pre;
+    tail.next = null;
+    length--;
+    if (length == 0) {
+      head = null;
+      tail = null;
+    }
+    return temp;
+  }
 }
+
+
