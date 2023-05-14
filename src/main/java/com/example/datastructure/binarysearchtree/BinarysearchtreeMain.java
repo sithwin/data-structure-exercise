642e84484515ec5426b8eb1e1a054ef9732d11a8
Si Thu Win <sthuwin@gmail.com>
2023-05-14 12:12:00 +1000

    binarysearchtree test

--- src/main/java/com/example/datastructure/binarysearchtree/BinarysearchtreeMain.java
+++ src/main/java/com/example/datastructure/binarysearchtree/BinarysearchtreeMain.java
@@ -1,2 +1,36 @@
-package com.example.datastructure.binarysearchtree;public class BinarysearchtreeMain {
+package com.example.datastructure.binarysearchtree;
+
+public class BinarysearchtreeMain {
+  public static void main(String[] args) {
+
+    BinarySearchTree myBST = new BinarySearchTree();
+
+    myBST.insert(47);
+    myBST.insert(21);
+    myBST.insert(76);
+    myBST.insert(18);
+    myBST.insert(27);
+    myBST.insert(52);
+    myBST.insert(82);
+
+
+    System.out.println("BST Contains 27:");
+    System.out.println(myBST.contains(27));
+
+    System.out.println("\nBST Contains 17:");
+    System.out.println(myBST.contains(17));
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            BST Contains 27:
+            true
+
+            BST Contains 17:
+            false
+
+        */
+
+  }
 }
