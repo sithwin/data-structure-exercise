4c742473ba0daa9c53cefdc327b75138f51e8806
Si Thu Win <sthuwin@gmail.com>
2023-05-16 12:12:00 +1000

    queue test

--- src/main/java/com/example/datastructure/queue/QueueMain.java
+++ src/main/java/com/example/datastructure/queue/QueueMain.java
@@ -1,2 +1,28 @@
-package com.example.datastructure.queue;public class QueueMain {
+package com.example.datastructure.queue;
+
+public class QueueMain {
+  public static void main(String[] args) {
+
+    Queue myQueue = new Queue(2);
+    myQueue.enqueue(1);
+
+    // (2) Items - Returns 2 Node
+    System.out.println(myQueue.dequeue().value);
+    // (1) Item - Returns 1 Node
+    System.out.println(myQueue.dequeue().value);
+    // (0) Items - Returns null
+    System.out.println(myQueue.dequeue());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            2
+            1
+            null
+
+        */
+
+  }
+
 }
