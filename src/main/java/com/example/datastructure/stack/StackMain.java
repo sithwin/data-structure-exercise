6fb7849c2b96ca485b004b6b619422b95a48a8ca
Si Thu Win <sthuwin@gmail.com>
2023-05-17 12:12:00 +1000

    stack test

--- src/main/java/com/example/datastructure/stack/StackMain.java
+++ src/main/java/com/example/datastructure/stack/StackMain.java
@@ -1,2 +1,27 @@
-package com.example.datastructure.stack;public class StackMain {
+package com.example.datastructure.stack;
+
+public class StackMain {
+  public static void main(String[] args) {
+
+    Stack myStack = new Stack(2);
+    myStack.push(1);
+
+    // (2) Items - Returns 1 Node
+    System.out.println(myStack.pop().value);
+    // (1) Item - Returns 2 Node
+    System.out.println(myStack.pop().value);
+    // (0) Items - Returns null
+    System.out.println(myStack.pop());
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            1
+            2
+            null
+
+        */
+
+  }
 }
