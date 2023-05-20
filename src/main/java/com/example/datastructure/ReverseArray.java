36aabfff853dd843a60278171d43f2692aa09403
Si Thu Win <sthuwin@gmail.com>
2023-05-20 21:48:21 +1000

    reverse array

---
+++ src/main/java/com/example/datastructure/ReverseArray.java
@@ -0,0 +1,22 @@
+package com.example.datastructure;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+  public static void reverse(int[] array) {
+    int length = array.length;
+    for (int i = 0; i < length / 2; i++) {
+      // right side
+      int rightIndex = array.length - 1 - i;
+      int left = array[i];
+      array[i] = array[rightIndex];
+      array[rightIndex] = left;
+    }
+    System.out.println(Arrays.toString(array));
+  }
+
+  public static void main(String[] args) {
+    int[] nums1 = {1, 2, 3, 4, 5, 6};
+    reverse(nums1);
+  }
+}
